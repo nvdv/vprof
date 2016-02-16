69f5ae448fcd7eca6bc3f6db7b90284bb1c7c955
nvdv <modestdev@gmail.com>
2016-02-16 12:47:44 +0300

    Add JSDoc and some tests to code_heatmap.js.

--- vprof/frontend/code_heatmap.js
+++ vprof/frontend/code_heatmap.js
@@ -1,5 +1,5 @@
 /**
- * Renders code heatmap.
+ * @file Code heatmap rendering.
  */
 
 /* jshint strict: false, browser: true, globalstrict: true */
@@ -10,7 +10,16 @@ var d3 = require('d3');
 var hljs = require('highlight.js');
 require('./highlight.css');  // Includes code highlighter CSS.
 
-/** Class constructor. */
+/**
+ * Represents code heatmap.
+ * @constructor
+ * @param {Object} parent - Parent element for code heatmap.
+ * @param {Object} data - Data for code heatmap rendering.
+ * @property {number} MIN_RUN_COUNT - Min value for line execution count.
+ * @property {number} MAX_RUN_COUNT - Max value for line execution count.
+ * @property {string} MIN_RUN_COLOR - Color that represents MIN_RUN_COUNT.
+ * @property {string} MAX_RUN_COLOR - Color that represents MAX_RUN_COUNT.
+ */
 function CodeHeatmap(parent, data) {
   this.MIN_RUN_COUNT = 0;
   this.MAX_RUN_COUNT = 2500;
@@ -45,15 +54,25 @@ CodeHeatmap.prototype.render = function() {
     .on('mouseout', function() { self.hideTooltip_(this, tooltip); });
 };
 
-/** Changes line background color based on execution count. */
+/**
+ * Returns line background color based on execution count.
+ * @param {Object} _ - Unused argument.
+ * @param {number} i - Line number.
+ * @returns {string}
+ */
 CodeHeatmap.prototype.changeBackgroundColor_ = function(_, i) {
   var runCount = this.data_.heatmap[i + 1];
   return runCount ? this.heatmapScale_(runCount) : '';
 };
 
-/** Shows line execution count inside tooltip and adds line highlighting. */
-CodeHeatmap.prototype.showTooltip_ = function(element, tooltip, index) {
-  var runCount = this.data_.heatmap[index + 1];
+/**
+ * Shows line execution count inside tooltip and adds line highlighting.
+ * @param {Object} element - Element representing highlighted line.
+ * @param {Object} tooltip - Element representing tooltip,
+ * @param {number} i - Source line number.
+ */
+CodeHeatmap.prototype.showTooltip_ = function(element, tooltip, i) {
+  var runCount = this.data_.heatmap[i + 1];
   if (runCount) {
     d3.select(element).attr('class', 'src-line-highlight');
     tooltip.attr('class', 'tooltip tooltip-visible')
@@ -63,14 +82,22 @@ CodeHeatmap.prototype.showTooltip_ = function(element, tooltip, index) {
   }
 };
 
-/** Hides provided tooltip and removes line highlighting. */
+/**
+ * Hides provided tooltip and removes line highlighting.
+ * @param {Object} element - Element representing highlighted line.
+ * @param {Object} tooltip - Element representing tooltip,
+ */
 CodeHeatmap.prototype.hideTooltip_ = function(element, tooltip) {
   d3.select(element).attr('class', 'src-line-normal');
   tooltip.attr('class', 'tooltip tooltip-invisible');
 };
 
-/** Adds line numbers and additional formatting since highlight.js does not
-    support them. */
+/**
+ * Adds line numbers and additional formatting since highlight.js does not
+ * support them.
+ * @param {string} srcCode - Python source code.
+ * @returns {string}
+ */
 CodeHeatmap.postProcessCode_ = function(srcCode) {
   var lines = srcCode.split('\n');
   for (var i = 1; i < lines.length + 1; i++) {
@@ -83,7 +110,11 @@ CodeHeatmap.postProcessCode_ = function(srcCode) {
   return lines.join('');
 };
 
-/** Factory function to call externally. */
+/**
+ * Renders code heatmap and attaches it to parent.
+ * @param {Object} parent - Parent element for code heatmap.
+ * @param {Object} data - Data for code heatmap rendering.
+ */
 function renderCodeHeatmap(data, parent) {
   var heatmap = new CodeHeatmap(parent, data);
   heatmap.render();
--- vprof/frontend/code_heatmap_test.js
+++ vprof/frontend/code_heatmap_test.js
@@ -1,9 +1,7 @@
 var code_heatmap = require('./code_heatmap.js');
 
 describe('Code heatmap test suite', function() {
-
-  it('Check getNodeName', function() {
-    var heatmap = new code_heatmap.CodeHeatmap();
+  it('Check postProcessCode_', function() {
     var srcCode = "def foo():\nreturn 'bar'";
     var expectedResult = (
       "<div class='src-line-normal'>" +
@@ -14,8 +12,18 @@ describe('Code heatmap test suite', function() {
         "<div class='src-line-number'>2</div>" +
         "<div class='src-line-code'>return 'bar'</div>" +
       "</div>");
-    expect(code_heatmap.CodeHeatmap.postProcessCode_(srcCode)).toBe(
-        expectedResult);
+
+    expect(
+      code_heatmap.CodeHeatmap.postProcessCode_(srcCode)).toBe(expectedResult);
   });
 
+  it('Check changeBackgroundColor_', function() {
+    var heatmap = new code_heatmap.CodeHeatmap();
+    heatmap.data_ = { 'heatmap': [0, 0, 2, 3] };
+    var unusedArg = 'foo';
+
+    expect(heatmap.changeBackgroundColor_(unusedArg, 0)).toBe('');
+    expect(heatmap.changeBackgroundColor_(unusedArg, 1)).not.toBe('');
+    expect(heatmap.changeBackgroundColor_(unusedArg, 2)).not.toBe('');
+  });
 });
