cb857c1c90e824ac05b7194729d06694ec7a6b7a
nvdv <modestdev@gmail.com>
2016-03-02 16:37:25 +0300

    Bump version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pip.download import PipSession
 
 setup(
     name='vprof',
-    version='0.21',
+    version='0.22',
     packages=['vprof'],
     description="Visual profiler for Python",
     url='http://github.com/nvdv/vprof',
