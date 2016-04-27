3d2a5172d0ec892c6f60bf56424ba44abd277d64
nvdv <modestdev@gmail.com>
2016-04-27 19:34:56 +0300

    Add e2e test for functions to code heatmap test suite.

--- vprof/tests/code_heatmap_e2e.py
+++ vprof/tests/code_heatmap_e2e.py
@@ -9,6 +9,7 @@ from six.moves import urllib
 
 from vprof import code_heatmap
 from vprof import stats_server
+from vprof import vprof_runtime
 
 # For Python 2 and Python 3 compatibility.
 try:
@@ -112,3 +113,38 @@ class CodeHeatmapImportedPackageEndToEndTest(unittest.TestCase):
             stats[1]['objectName'], 'vprof/tests/test_pkg/dummy_module.py')
         self.assertListEqual(stats[0]['srcCode'], _MAIN_MODULE_SOURCELINES)
         self.assertListEqual(stats[1]['srcCode'], _DUMMY_MODULE_SOURCELINES)
+
+
+class CodeHeatmapFunctionEndToEndTest(unittest.TestCase):
+
+    def setUp(self):
+
+        def _func(foo, bar):
+            baz = foo + bar
+            return baz
+        self._func = _func
+
+        stats_handler = functools.partial(
+            stats_server.StatsHandler, {})
+        self.server = stats_server.StatsServer(
+            (_HOST, _PORT), stats_handler)
+        threading.Thread(target=self.server.serve_forever).start()
+
+    def tearDown(self):
+        self.server.shutdown()
+        self.server.server_close()
+
+    def testRequest(self):
+        vprof_runtime.run(
+            self._func, 'h', ('foo', 'bar'), host=_HOST, port=_PORT)
+        response = urllib.request.urlopen(
+            'http://%s:%s/profile' % (_HOST, _PORT))
+        stats = json.loads(response.read().decode('utf-8'))
+        self.assertEqual(len(stats), 1)
+        self.assertTrue('function _func' in stats['h'][0]['objectName'])
+        self.assertDictEqual(
+            stats['h'][0]['heatmap'], {'123': 1, '124': 1})
+        self.assertListEqual(stats['h'][0]['srcCode'],
+            [[122, '        def _func(foo, bar):\n'],
+             [123, u'            baz = foo + bar\n'],
+             [124, u'            return baz\n']])
