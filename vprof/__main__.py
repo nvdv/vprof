fe40a3a47ad7d9381efb14c4f664e3f98fbc97f7
nvdv <aflatnine@gmail.com>
2017-09-26 16:44:44 +0300

    Bump version.

--- vprof/__main__.py
+++ vprof/__main__.py
@@ -18,7 +18,7 @@ import sys
 from vprof import runner
 from vprof import stats_server
 
-__version__ = '0.38.dev0'
+__version__ = '0.37.1'
 
 _PROGRAN_NAME = 'vprof'
 _MODULE_DESC = 'Visual profiler for Python'
